68f95519b4cc6404e8be674cc86f06fd25311cbe
jyates722 <53183091+jyates722@users.noreply.github.com>
2019-07-29 12:56:57 -0400

    Add files via upload

---
+++ day5/1-10.js
@@ -0,0 +1,33 @@
+console.log('1');
+console.log('2');
+console.log('3');
+console.log('4');
+console.log('5');
+console.log('6');
+console.log('7');
+console.log('8');
+console.log('9');
+console.log('10');
+
+console.log('1\n 2 \n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10');
+
+let result =prompt ("enter your name");
+console.log("hello,", result);
+//let num = prompt("enter number"); 
+//num = Number (num);
+//if (isNaN(num))
+//{
+//    num = 10;
+//}
+//num = num + 10;
+//console.log(num);
+
+    let response =prompt ("What is your grade?");
+
+    let grade =Number(response);
+
+if(grade>= 70);
+
+    console.log('you passed');
+
+    console.log('you failed'); 
---
+++ day5/script2.js
@@ -0,0 +1,56 @@
+// practicing functions 
+
+// f(x) is a function that 
+//takes a single 
+//
+
+
+function f (x)
+{
+     return 3 * x + 7;
+}
+
+console.log(f(2));
+console.log(f(12));
+console.log(f(7))
+
+
+
+function g (x) 
+{
+ return 5*x + 5; 
+}
+
+console.log (g(1));
+console.log(g(2))
+console.log(g(4))
+
+function h (x) 
+{
+    return 5*x**2 - 4*x + 2 
+}
+
+console.log(h(1))
+
+function i (x,y)
+{
+   return x*x - y*y; 
+}
+console.log(i(3,4));
+console.log(i(4,3));
+console.log(i(5,7));
+
+function j(x,y,z){
+    return x + y + z 
+}
+
+console.log(j(3,2,1));
+console.log(j(1,2,3));
+    console.log(j(2,1,3));
+
+    function k(r)
+    {
+return 2*3.14
+    }
+
+    console.log
